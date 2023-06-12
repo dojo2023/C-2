c5a513f2cda0637a4a5f215729d0e2e7284406bb
dojo2023-takebe <130331163+dojo2023-takebe@users.noreply.github.com>
2023-06-12 15:22:26 +0900

    a

--- src/src/servlet/New_memberServlet.java
+++ src/src/servlet/New_memberServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,8 +20,9 @@ public class New_memberServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// 新規会員登録画面へフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/new_member.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
--- src/src/servlet/Time_lineServlet.java
+++ src/src/servlet/Time_lineServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,9 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class New_memberServlet
+ * Servlet implementation class Time_lineServlet
  */
-@WebServlet("/New_memberServlet")
+@WebServlet("/Time_lineServlet")
 public class Time_lineServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
