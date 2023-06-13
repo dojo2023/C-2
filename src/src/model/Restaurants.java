13939550abe7d3a7fcff3604b98388aed5410fd0
dojo2023-yoshimoto <130334570+dojo2023-yoshimoto@users.noreply.github.com>
2023-06-13 14:35:30 +0900

    model吉本分完成

--- src/src/model/Restaurants.java
+++ src/src/model/Restaurants.java
@@ -4,4 +4,49 @@ import java.io.Serializable;
 
 public class Restaurants implements Serializable {
 
+	private String posts_restaurant;
+	private String walk;
+	private String serve;
+	private String price;
+	private String posts_genre;
+
+	public Restaurants(String posts_restaurant,String walk,String serve,String price,String posts_genre) {
+		super();
+		this.posts_restaurant = posts_restaurant;
+		this.walk = walk;
+		this.serve = serve;
+		this.price = price;
+		this.posts_genre = posts_genre;
+	}
+
+	public String getPosts_restaurant() {
+		return posts_restaurant;
+	}
+	public void setPosts_restaurant(String posts_restaurant) {
+		this.posts_restaurant = posts_restaurant;
+	}
+	public String getWalk() {
+		return walk;
+	}
+	public void setWalk(String walk) {
+		this.walk = walk;
+	}
+	public String getServe() {
+		return serve;
+	}
+	public void setServe(String serve) {
+		this.serve = serve;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getPosts_genre() {
+		return posts_genre;
+	}
+	public void setPosts_genre(String posts_genre) {
+		this.posts_genre = posts_genre;
+	}
 }
--- src/src/model/Users.java
+++ src/src/model/Users.java
@@ -3,13 +3,22 @@ package model;
 import java.io.Serializable;
 
 public class Users implements Serializable {
+	private String id;
 	private String mail_address;
 	private String password;
 
-	public Users(String mail_address,String password) {
+	public Users(String id, String mail_address,String password) {
 		super();
 		this.mail_address = mail_address;
 		this.password = password;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public String getMail_address() {
