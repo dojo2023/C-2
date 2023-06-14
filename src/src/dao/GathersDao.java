5ff43b336193b11d9dead2861a4bb6244960c941
dojo2023-ishiharaS <130334435+dojo2023-ishiharaS@users.noreply.github.com>
2023-06-14 15:20:24 +0900

    s

--- src/src/dao/GathersDao.java
+++ src/src/dao/GathersDao.java
@@ -4,15 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
-import model.Bc;
 import model.Gathers;
 
 public class GathersDao {
-	// 引数boshuで指定されたレコードを登録し、成功したらtrueを返す
- 	public boolean insert(buster_moon gathers) { //登録メソッド
+	//募集をかける
+	//"insert into gathers values (?, ?, ?, ?)"
+ 	public boolean insert(Gathers gather) { //登録メソッド
  		Connection conn = null;
  		boolean result = false;
  		try {
@@ -20,148 +18,110 @@ public class GathersDao {
 			Class.forName("org.h2.Driver");
 
 			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/buster_moon", "sa", "");
 
 			// SQL文を準備する
-			String sql = "insert into BC values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			String sql = "insert into GATHERS values (?, ?, ?, ?)";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
 			// SQL文を完成させる
-			if (card.getNumber() != null && !card.getNumber().equals("")) {
-				pStmt.setString(1, card.getNumber());
+			if (gather.getId() != null && !gather.getId().equals("")) {
+				pStmt.setString(1, gather.getId());
 			}
 			else {
 				pStmt.setString(1, null);
 			}
-	try {
-		// JDBCドライバを読み込む
-		Class.forName("org.h2.Driver");
+			if (gather.getUsers_id() != null && !gather.getUsers_id().equals("")) {
+				pStmt.setString(2, gather.getUsers_id());
+			}
+			else {
+				pStmt.setString(2, null);
+			}
+			if (gather.getDate() != null && !gather.getDate().equals("")) {
+				pStmt.setString(3, gather.getDate());
+			}
+			else {
+				pStmt.setString(3, null);
+			}
+			if (gather.getPoint() != null && !gather.getPoint().equals("")) {
+				pStmt.setString(4, gather.getPoint());
+			}
+			else {
+				pStmt.setString(4, null);
+			}
+			// SQL文を実行する
+						if (pStmt.executeUpdate() == 1) {
+							result = true;
+						}
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+					catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+					finally {
+						// データベースを切断
+						if (conn != null) {
+							try {
+								conn.close();
+							}
+							catch (SQLException e) {
+								e.printStackTrace();
+							}
+						}
+					}
 
-		// データベースに接続する
-		conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/buster_moon", "sa", "");
+	// 結果を返す
+	return result;
+ 	}
 
-		// SQL文を準備する(募集をかける)
-        String sql = "insert into gathers values (?, ?, ?)";
+ 	//募集の削除
+ 		//"delete from gathers where id = ?"
+	// 引数idで指定されたレコードを削除し、成功したらtrueを返す
+		public boolean delete(String id) {
+			Connection conn = null;
+			boolean result = false;
 
-        PreparedStatement pStmt = conn.prepareStatement(sql);
-
-		// SQL文を完成させる
-		if (.getId() != null && !card.getNumber().equals("")) {
-			pStmt.setString(1, card.getNumber());
-		}
-		else {
-			pStmt.setString(1, null);
-		}
-
-		if (card.getCompany() != null && !card.getCompany().equals("")) {
-			pStmt.setString(2, card.getCompany());
-		}
-		else {
-			pStmt.setString(2, null);
-		}
-
-		if (card.getDepartment() != null && !card.getDepartment().equals("")) {
-			pStmt.setString(3, card.getDepartment());
-		}
-		else {
-			pStmt.setString(3, null);
-		}
-
-		if (card.getPosition() != null && !card.getPosition().equals("")) {
-			pStmt.setString(4, card.getPosition());
-		}
-		else {
-			pStmt.setString(4, null);
-		}
-
-		if (card.getName() != null && !card.getName().equals("")) {
-			pStmt.setString(5, card.getName());
-		}
-		else {
-			pStmt.setString(5, null);
-		}
-
-		if (card.getRubi() != null && !card.getRubi().equals("")) {
-			pStmt.setString(6, card.getRubi());
-		}
-		else {
-			pStmt.setString(6, null);
-		}
-
-		if (card.getZipcode() != null && !card.getZipcode().equals("")) {
-			pStmt.setString(7, card.getZipcode());
-		}
-		else {
-			pStmt.setString(7, null);
-		}
-
-		if (card.getAddress() != null && !card.getAddress().equals("")) {
-			pStmt.setString(8, card.getAddress());
-		}
-		else {
-			pStmt.setString(8, null);
-		}
-
-		if (card.getPhone() != null && !card.getPhone().equals("")) {
-			pStmt.setString(9, card.getPhone());
-		}
-		else {
-			pStmt.setString(9, null);
-		}
-
-		if (card.getFax() != null && !card.getFax().equals("")) {
-			pStmt.setString(10, card.getFax());
-		}
-		else {
-			pStmt.setString(10, null);
-		}
-
-		if (card.getEmail() != null && !card.getEmail().equals("")) {
-			pStmt.setString(11, card.getEmail());
-		}
-		else {
-			pStmt.setString(11, null);
-		}
-
-		if (card.getRemarks() != null && !card.getRemarks().equals("")) {
-			pStmt.setString(12, card.getRemarks());
-		}
-		else {
-			pStmt.setString(12, null);
-		}
-
-
-		// SQL文を実行する
-		if (pStmt.executeUpdate() == 1) {
-			result = true;
-		}
-	}
-	catch (SQLException e) {
-		e.printStackTrace();
-	}
-	catch (ClassNotFoundException e) {
-		e.printStackTrace();
-	}
-	finally {
-		// データベースを切断
-		if (conn != null) {
 			try {
-				conn.close();
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/buster_moon", "sa", "");
+
+				// SQL文を準備する
+				String sql = "delete from gathers where id = ?";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				pStmt.setString(1, id);
+
+				// SQL文を実行する
+				if (pStmt.executeUpdate() == 1) {
+					result = true;
+				}
 			}
 			catch (SQLException e) {
 				e.printStackTrace();
 			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// 結果を返す
+			return result;
 		}
-	}
-
-	// 結果を返す
-	return result;
 }
 
-	//募集をかける
-	//"insert into gathers values (?, ?, ?)"
-
-	//募集の削除
-	//"delete from gathers where id = ?"
-
-}
--- src/src/dao/RestaurantsDao.java
+++ src/src/dao/RestaurantsDao.java
@@ -1,11 +1,117 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Restaurants;
+
 public class RestaurantsDao {
 
 	//検索
-	//"select * from restaurants WHERE posts_restaurant like ?, walk = ?, serve = ?, price = ?, post = ? ORDER BY post_resutaurant"
+	//"select * from restaurants WHERE posts_restaurant like '%?%'and walk = ? and  serve = ? and price = ? and
+	// posts_genre = ? ORDER BY posts_resutaurant"
+	// 引数paramで検索項目を指定し、検索結果のリストを返す
+		public List<Restaurants> select(Restaurants param) {
+			Connection conn = null;
+			List<Restaurants> restaurantList = new ArrayList<Restaurants>();
 
-	//店舗詳細の表示
-	//"select * from restaurants WHERE posts_restaurant = ?"
-	//"select photo, text from posts where restaurant = ?"
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/buster_moon", "sa", "");
+
+				// SQL文を準備する
+				String sql = "select * from restaurants WHERE posts_restaurant like '%?%'and walk = ? and  serve = ? "
+						+ "and price = ? and posts_genre = ? ORDER BY posts_resutaurant";
+				PreparedStatement pStmt = conn.prepareStatement(sql); //検索メソッド
+
+				// SQL文を完成させる
+				if (param.getPosts_restaurant() != null) {
+					pStmt.setString(1, "%" + param.getPosts_restaurant() + "%");
+				}
+				else {
+					pStmt.setString(1, "%");
+				}
+				if (param.getWalk() != null) {
+					pStmt.setString(2, "%" + param.getWalk() + "%");
+				}
+				else {
+					pStmt.setString(2, "%");
+				}
+				if (param.getServe() != null) {
+					pStmt.setString(3, "%" + param.getServe() + "%");
+				}
+				else {
+					pStmt.setString(3, "%");
+				}
+				if (param.getPrice() != null) {
+					pStmt.setString(4, "%" + param.getPrice() + "%");
+				}
+				else {
+					pStmt.setString(4, "%");
+				}
+				if (param.getPosts_genre() != null) {
+					pStmt.setString(5, "%" + param.getPosts_genre() + "%");
+				}
+				else {
+					pStmt.setString(5, "%");
+				}
+				// SQL文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+
+				// 結果表をコレクションにコピーする
+				while (rs.next()) {
+					Restaurants retaurant = new Restaurants(
+					rs.getString("POSTS_RESTAURANT"),
+					rs.getString("WALK"),
+					rs.getString("SERVE"),
+					rs.getString("PRICE"),
+					rs.getString("POSTS_GENRE")
+					);
+					restaurantList.add(retaurant);
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				restaurantList = null;
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				restaurantList = null;
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						restaurantList = null;
+					}
+				}
+			}
+
+			// 結果を返す
+			return restaurantList;
+		}
+
+		//店舗詳細の表示
+		//"select * from restaurants WHERE posts_restaurant = '?'"
+
+
+		//"select photo, text from posts where restaurant = '?'"
+
+
+
+
+
+
 }
